2c2fec55d5ec0020685ee348ebdddf47370c7098
maxSt <stovpovymax@gmail.com>
2023-02-08 19:56:54 -0500

    readme.md generator v1.0.0

---
+++ README.md
@@ -0,0 +1,16 @@
+# README.md generator
+[Description](#description)---[License](#license)---[Questions](#questions)
+
+## Description
+Using this application.\
+You can easily create a README.md file for your repository.
+
+## License
+
+The Unlicense [more info](https://choosealicense.com/licenses/)
+
+## Questions
+
+For communication use:\
+git: MaksymStovpovyi\
+email: stovpovymax@gmail.com
---
+++ index.js
@@ -0,0 +1,59 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Enter project name ==>',
+      name: 'projName',
+    },
+    {
+      type: 'input',
+      message: 'Enter description of project ==>',
+      name: 'desc',
+    },
+    {
+      type: 'list',
+      message: 'Choose a license',
+      choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License'],
+      name: 'license',
+    },
+    {
+      type: 'input',
+      message: 'Enter your GitHub username',
+      name: 'gitName',
+    },
+    {
+      type: 'input',
+      message: 'Enter your email ==>',
+      name: 'email',
+    },
+    
+  ])
+  .then((response) => {
+    const { projName, desc, license, gitName, email } = response;
+    console.log(response);
+    fs.writeFile('README-generator.md', `# ${projName}
+[Description](#description)---[License](#license)---[Questions](#questions)\\
+
+## Description
+${desc}
+
+## License
+
+${license} [more info](https://choosealicense.com/licenses/)
+
+## Questions
+
+For communication use:\\
+git: ${gitName}\\
+email: ${email}
+
+    `, (err) =>
+    
+    err ? console.error(err) : console.log('Commit logged!')
+    );
+
+    }
+  );
